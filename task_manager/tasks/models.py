6985b76704322120d659a233227b313fba6b58fc
na07 <nkef2698@gmail.com>
2024-07-06 13:08:26 +0300

    added models: Task make migrations

---
+++ task_manager/tasks/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Task(models.Model):
+    STATUS_CHOICE = [
+        ('pemding', 'Panding'),
+        ('in_progress', 'In Progress'),
+        ('completed', 'Completed'),
+    ]
+
+    PRIORITY_CHOICES = [
+        ('low', 'Low'),
+        ('medium', "Medium"),
+        ('high', "High"),
+    ]
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICE, default='pending')
+    priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES, default='low')
+    dve_date = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title 
+    
+
+class Comment(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    create_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
